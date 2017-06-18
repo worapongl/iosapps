981a0799c562eb9303d5beb62472b98735da2ae8
Worapong Lekuthaiphan <worapong.l@dimensiondata.com>
2017-06-19 01:38:52 +0700

    R1

--- favorite-movies/favorite-movies/ViewController.swift
+++ favorite-movies/favorite-movies/ViewController.swift
@@ -8,9 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    //var favoriteMovies: [Movie]
+    var favoriteMovies: [Movie] = []
+    
+    @IBOutlet var mainTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moviecell = tableView.dequeueReusableCell(withIdentifier: "customcell",
+                                                      for: indexPath) as! CustomTableViewCell
+        
+        let idx: Int = indexPath.row
+        
+        moviecell.movieTitle?.text = favoriteMovies[idx].title
+        moviecell.movieYear?.text = favoriteMovies[idx].year
+        displayMovieImage(idx, moviecell: moviecell)
+        return moviecell
+        
+    }
+    
+    func displayMovieImage(_ row: Int, moviecell: CustomTableViewCell){
+        let url: String = (URL(string: favoriteMovies[row].imageUrl)?.absoluteString)!
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { (data,response,error)->Void in
+            if error != nil{
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async(execute:  {
+                let image = UIImage(data: data!)
+                moviecell.movieImageView?.image = image
+                
+            })
+        }).resume()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        mainTableView.reloadData()
+        if favoriteMovies.count == 0{
+            favoriteMovies.append(Movie(id: "tt3322234", title: "Batman Begin", year: "2017", imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51FNDCZH83L._AC_UL115_.jpg"))
+        }
+        
+        super.viewWillAppear(animated)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
